a514e4b608d2a47d3ca58c71ff8074af2fdd76ea
alexnsouza6 <alexnsouza6@hotmail.com>
2018-10-21 12:35:23 -0200

    refactor: rubocop in action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,46 +1,54 @@
 class UsersController < ApplicationController
+  before_action :require_user, except: %i[index show]
+  before_action :require_same_user, only: %i[edit update destroy]
 
-	def index
-		@users = User.all.paginate(page: params[:page], per_page: 3)
-	end
+  def index
+    @users = User.all.paginate(page: params[:page], per_page: 3)
+  end
 
-	def new
-		@user = User.new
-	end
+  def new
+    @user = User.new
+  end
 
-	def create
-		@user = User.new(user_params)
-		if @user.save
-			flash[:success] = "Welcome to Alpha Blog #{@user.username}"
-			redirect_to articles_path
-		else
-			render 'new'
-		end
-	end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to Alpha Blog, #{@user.username}"
+      redirect_to articles_path
+    else
+      render 'new'
+    end
+  end
 
-	def edit
-		@user = User.find(params[:id])	
-	end
+  def edit
+    @user = User.find(params[:id])
+  end
 
-	def update
-		@user = User.find(params[:id])	
-		if @user.update(user_params)
-			flash[:success] = "Your account was successfully updated"
-			redirect_to articles_path
-		else
-			render 'edit'
-		end
-	end
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = 'Your account was successfully updated'
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+  end
 
-	def show
-		@user = User.find(params[:id])
-		@user_articles = @user.articles.paginate(page: params[:page], per_page: 3)
-	end
+  def show
+    @user = User.find(params[:id])
+    @user_articles = @user.articles.paginate(page: params[:page], per_page: 3)
+  end
 
+  private
 
-	private
-		def user_params
-			params.require(:user).permit(:username,:email,:password)
-		end
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 
+  def require_same_user
+    if current_user != @user
+      flash[:danger] = 'You can only edit your own profile!'
+      redirect_to root_path
+    end
+  end
 end
