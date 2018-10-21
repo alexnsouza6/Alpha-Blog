f7c2f5dd5786279ff1472ef5f259a7500ccd0a19
alexnsouza6 <alexnsouza6@hotmail.com>
2018-10-21 13:28:13 -0200

    feat: Add admin funcionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :require_user, except: %i[index show]
   before_action :require_same_user, only: %i[edit update destroy]
+  before_action :require_admin, only: :destroy
 
   def index
     @users = User.all.paginate(page: params[:page], per_page: 3)
@@ -40,6 +41,13 @@ class UsersController < ApplicationController
     @user_articles = @user.articles.paginate(page: params[:page], per_page: 3)
   end
 
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    flash[:danger] = 'User was deleted successfully'
+    redirect_to users_path
+  end
+
   private
 
   def user_params
@@ -47,9 +55,17 @@ class UsersController < ApplicationController
   end
 
   def require_same_user
-    if current_user != @user
+    if current_user != @user && !current_user.admin?
       flash[:danger] = 'You can only edit your own profile!'
       redirect_to root_path
     end
   end
+
+  def require_admin
+    if logged_in? && !current_user.admin?
+      flash[:danger] = 'Only Admins can destroy users profile'
+      redirect_to root_path
+    end
+  end
+
 end
