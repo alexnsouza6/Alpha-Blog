bd64d757af8baddcb53000d5a9f87117d70ae989
alexnsouza6 <alexnsouza6@hotmail.com>
2018-10-21 12:34:36 -0200

    feat: Adding restrictions in UI

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,26 +1,23 @@
+# Sessions Controller handles Sessions stuff
 class SessionsController < ApplicationController
+  def new 
+  end
 
-	def new
-	
-	end
-
-	def create
-  user = User.find_by(email: params[:session][:email].downcase)
-		if user && user.authenticate(params[:session][:password])
-			session[:user_id] = user.id # Saving user id in the session. The browser cookies handles this
-			flash[:success] = 'Logged in Successufully'
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+	  if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id # Saving user id in the session. The browser cookies handles this
+      flash[:success] = 'Logged in Successufully'
 			redirect_to user_path(user)
 		else
 			flash.now[:danger] = 'Something wrong with login info'
 			render 'new'
 		end
-
-	end
+  end
 
 	def destroy
 		session[:user_id] = nil
 		flash[:success] = "You have logged out"
 		redirect_to root_path
 	end
-
 end
