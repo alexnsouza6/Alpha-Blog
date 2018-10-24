dcf0b8ae3bf1b19f2a181f89ea65123795909739
alexnsouza6 <alexnsouza6@hotmail.com>
2018-10-24 20:03:05 -0200

    feat: Developing category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
 # Every Article has a Category. Thus, a category has a name with a fixed length.
-class Categorie < ApplicationRecord
+class Category < ApplicationRecord
   validates :name, presence: true, length: { minimum: 3, maximum: 25}
-  validate_uniqueness_of :name
+  validates_uniqueness_of :name
 end
