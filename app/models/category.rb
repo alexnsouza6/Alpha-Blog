44d1bd0aa54c6e2bfcc103d5ed66e9eee4a2cc95
alexnsouza6 <alexnsouza6@hotmail.com>
2018-10-25 00:10:56 -0200

    feat: model associations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,7 @@
 # Every Article has a Category. Thus, a category has a name with a fixed length.
 class Category < ApplicationRecord
+  has_many :article_categories
+  has_many :articles, through: :article_categories
   validates :name, presence: true, length: { minimum: 3, maximum: 25}
   validates_uniqueness_of :name
 end
