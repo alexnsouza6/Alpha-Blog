21221cd9eab6c7acbe51a6fceaaa5afffecea3b2
alexnsouza6 <alexnsouza6@hotmail.com>
2018-10-24 22:59:50 -0200

    refactor: rubocop

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,10 @@
 # Sessions Controller handles Sessions stuff
 class SessionsController < ApplicationController
-  def new 
-  end
+  def new; end
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-	  if user && user.authenticate(params[:session][:password])
+		if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id # Saving user id in the session. The browser cookies handles this
       flash[:success] = 'Logged in Successufully'
 			redirect_to user_path(user)
