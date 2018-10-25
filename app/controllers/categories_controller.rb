908e3ac87fd02cb1d9b6dfd512f8194b641dc2ae
alexnsouza6 <alexnsouza6@hotmail.com>
2018-10-24 22:59:32 -0200

    refactor: deleting '.all' before paginate method

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,20 +1,37 @@
+# Categories Controller is composed by a simple CRUD of category
 class CategoriesController < ApplicationController
-    def new
-      @category = Category.new
-    end
+  before_action :require_admin, only: %i[new create]
 
-    def create
-      @category = Category.new(category_params)
-      if @category.save
-          flash[:success] = 'The Category has been successfully created!'
-          redirect_to root_path
-      else
-          render 'new'
-      end
-    end
+  def index
+    @categories = Category.paginate(page: params[:page], per_page: 3)
+  end
 
-    private
-        def category_params
-          params.require(:category).permit(:name)
-        end
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:success] = 'The Category has been successfully created!'
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  def show; end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+
+  def require_admin
+    if !logged_in? || (logged_in? && !current_user.admin?)
+      flash[:danger] = 'You have to be an Admin to perform this action.'
+      redirect_to root_path
+    end
+  end
 end
